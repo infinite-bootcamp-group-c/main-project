4ce7630bc5a8f385e904a0d193aa51b853fc65b3
Homayoun Zarei <homayounzr@gmail.com>
2022-08-24 16:29:38 +0430

    added IBaseRepository and ABaseRepository

--- src/Lib/Repository/ABaseRepository.php
+++ src/Lib/Repository/ABaseRepository.php
@@ -2,7 +2,34 @@
 
 namespace App\Lib\Repository;
 
-class ABaseRepository
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @extends ServiceEntityRepository
+ *
+ * @method find($id, $lockMode = null, $lockVersion = null)
+ * @method findOneBy(array $criteria, array $orderBy = null)
+ * @method array findAll()
+ * @method array findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+abstract class ABaseRepository extends ServiceEntityRepository implements IBaseRepository
 {
+    public function add($entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove($entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 
 }
