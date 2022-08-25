6b92d681eb0165ff147aa02f661bbc7a77c4eb7c
mehrab <mehrabmhosseini@gmail.com>
2022-08-25 14:26:25 +0430

    add new functions to createProductView

--- src/View/Product/CreateProductView.php
+++ src/View/Product/CreateProductView.php
@@ -2,22 +2,69 @@
 
 namespace App\View\Product;
 
-use App\Entity\User;
+use App\Entity\Product;
 use App\Lib\View\ACreateView;
-use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ProductRepository;
+use DateTime;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class CreateProductView extends ACreateView
 {
-
-    protected function createObject(array $form): array
+    public function __construct(private ProductRepository $productRepository)
     {
-        // TODO: Implement getData() method.
-        return [];
     }
 
-    public function execute(array $params): static
+    protected function createObject(array $form): Product
     {
-        // TODO: Implement getData() method.
-        return new static();
+        /**
+         * creates a Product entity
+         */
+
+        $product = new Product();
+        static::setProductProperties($product, $form);
+        $this->productRepository->add($product, true);
+        return $product;
+    }
+
+    private function setProductProperties(Product $product, array $form): void
+    {
+        /**
+         * Sets all properties of a product to the entity with their setter methods
+         * TODO: add product photos
+         */
+
+        $product->setName($form["body"]["createdAt"]);
+        $product->setPrice($form["body"]["price"]);
+        $product->setCategory($form["body"]["category"]);
+        $product->setDescription($form["body"]["description"]);
+        $product->setQuantity($form["body"]["quantity"]);
+        $product->setName($form["body"]["name"]);
+        $product->setCreatedAt(new DateTime());
+        $product->setUpdatedAt(new DateTime());
+    }
+
+    public function createResponse(array $responseArray): JsonResponse
+    {
+        /**
+         * creates Final response with given data and status
+         */
+
+        $response = new JsonResponse($responseArray);
+        $response->setStatusCode(201, "new product created");
+        return $response;
+    }
+
+
+    public static function execute(array $params): void
+    {
+        /**
+         * it is called by a form object
+         */
+        $instance = new static();
+        $product = $instance->createObject($params);
+        $productArray = $instance->toArray($product);
+        $response = $instance->createResponse($productArray);
+        $response->send();
     }
 }
