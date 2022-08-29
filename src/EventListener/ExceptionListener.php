f37b806d1b90cc4ce9144c68cc03e7d9a45c3b99
Amin Rasouli <me@aminrasouli.ir>
2022-08-29 14:42:17 +0430

    exception filter

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $request = $event->getRequest();
+
+        $response = new JsonResponse([
+            'message' => $exception->getMessage(),
+            'code' => $exception->getCode(),
+            'traces' => $exception->getTrace()
+        ]);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        $event->setResponse($response);
+    }
+}
