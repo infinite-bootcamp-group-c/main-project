7247c058d33e58fe8160e34cc70701cc10ca95aa
Amin Rasouli <me@aminrasouli.ir>
2022-08-29 11:31:31 +0430

    move makeResponse to form

--- src/Lib/Controller/BaseController.php
+++ src/Lib/Controller/BaseController.php
@@ -2,19 +2,8 @@
 
 namespace App\Lib\Controller;
 
-use App\Lib\Form\IBaseForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class BaseController extends AbstractController
 {
-    public function makeResponse(IBaseForm $form, Request $request): JsonResponse
-    {
-        $validation = $form->validate($request);
-        if (count($validation))
-            return $this->json(['errors' => $validation]);
-        $jsonResult = $form->execute($request);
-        return $this->json($jsonResult);
-    }
 }
