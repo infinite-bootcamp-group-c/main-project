05deb2b8eefac8f4e2561c3e6d8cc67f2efa0061
hsmmi <h.mousavi9877@gmail.com>
2022-08-31 15:45:59 +0430

    Install the required dependencies Update the application's configuration DoctrineFixturesBundle

--- src/DataFixtures/ShopFixtures.php
+++ src/DataFixtures/ShopFixtures.php
@@ -2,7 +2,39 @@
 
 namespace App\DataFixtures;
 
-class ShopFixtures
+use App\Entity\Shop;
+use App\Repository\UserRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+
+class ShopFixtures extends Fixture implements FixtureGroupInterface
 {
 
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // create 3 shops! Bam!
+        for ($i = 1; $i < 4; $i++) {
+            $shop = new Shop();
+            $shop->setName('user'.$i);
+            $shop->setUser($this->userRepository->find(mt_rand(1, 3)));
+            $shop->setDescription('Test description for shop'.$i);
+            $shop->setIgUsername('ig'.$i);
+
+            $manager->persist($shop);
+        }
+
+        $manager->flush();
+    }
+
+
+    public static function getGroups(): array
+    {
+        return ['shop'];
+    }
 }
