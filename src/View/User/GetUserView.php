780b7b4abc24117e2845e67d755334e2cee667ea
Homayoun Zarei <homayounzr@gmail.com>
2022-09-03 06:30:18 +0430

    added profile controller for managing addresses and credit info

---
+++ src/View/User/GetUserView.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\View\User;
+
+use App\Entity\User;
+use App\Lib\View\ABaseView;
+use Symfony\Component\HttpFoundation\Response;
+
+class GetUserView extends ABaseView
+{
+    protected int $HTTPStatusCode = Response::HTTP_CREATED;
+
+    public function execute(User $user): array
+    {
+        return [
+            'id' => $user->getId(),
+            'phone_number' => $user->getPhoneNumber(),
+            'first_name' => $user->getFirstName(),
+            'last_name' => $user->getLastName(),
+            'email' => $user->getEmail(),
+        ];
+    }
+}
